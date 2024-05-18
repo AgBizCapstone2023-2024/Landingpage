a19903db998e94c7196e728acec8eb88267bcc7c
Randy-Osborn <randyosborn1989@hotmail.com>
2024-05-17 21:27:12 -0700

    update config

--- portfolio/next.config.mjs
+++ portfolio/next.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Add any other Next.js configuration options here
-
+output: "export",  // <=== enables static exports
+  reactStrictMode: true,
   images: {
     unoptimized: true, // Necessary because GitHub Pages doesn't support dynamic image optimization
   },
