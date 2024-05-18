7b68cc5667f2b6e90437e7ea408ccef49aca47e3
Randy-Osborn <randyosborn1989@hotmail.com>
2024-05-17 21:22:29 -0700

    update links

--- portfolio/src/app/layout.js
+++ portfolio/src/app/layout.js
@@ -9,7 +9,7 @@ export default function Layout({ children }) {
         <nav className="bg-gradient-to-r from-black to-gray-600 shadow">
           <ul className="flex justify-between p-4">
             <li className="flex space-x-4">
-              <a href="/" className="text-blue-500 hover:text-blue-700">Home</a>
+              <a href="" className="text-blue-500 hover:text-blue-700">Home</a>
               <a href="https://www.agbizlogic.com/index/#bottom" className="text-blue-500 hover:text-blue-700">Contact us</a>
             </li>
           </ul>
