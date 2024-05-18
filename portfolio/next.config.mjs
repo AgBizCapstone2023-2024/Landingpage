846d442056f6f780269d3d7cc16caae2e60efba6
Randy-Osborn <randyosborn1989@hotmail.com>
2024-05-17 19:11:36 -0700

    Update nextjs config and yaml

--- portfolio/next.config.mjs
+++ portfolio/next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true, // Necessary because GitHub Pages doesn't support dynamic image optimization
+  },
+  basePath: '/Landingpage', // Replace with the actual repository name
+};
 
 export default nextConfig;
