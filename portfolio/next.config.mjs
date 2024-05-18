b66e98ef716d5e0ec889f7f66841a7212e5af63f
Randy-Osborn <randyosborn1989@hotmail.com>
2024-05-17 19:38:49 -0700

    Change target

--- portfolio/next.config.mjs
+++ portfolio/next.config.mjs
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Configure static exports
-  // Set target to 'static' for static site generation
-  // This generates HTML files for each route
-  target: 'static',
-
   // Add any other Next.js configuration options here
 
   images: {
